e763a98493d0463a5449c80cde25c5ea23ffdbb2
Thomas <Thomas@LAPTOP-A96M754A>
2019-05-02 15:39:47 +0200

    Phase 2

--- src/Controller/IOrderPerformer.java
+++ src/Controller/IOrderPerformer.java
@@ -1,5 +1,5 @@
 package Controller;
 
 public interface IOrderPerformer {
-
+	public void orderPerform(userOrder : UserOrder);
 }
--- src/Controller/IUserOrder.java
+++ src/Controller/IUserOrder.java
@@ -1,5 +1,11 @@
 package Controller;
 
 public interface IUserOrder {
-
+	public getOrder() : Order;
+	public getPlayer() : int;
+	
+	public Order getOrder()
+	{
+		return Order;
+	}
 }
